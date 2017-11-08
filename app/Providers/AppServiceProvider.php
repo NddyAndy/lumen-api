4344d1f2c166f43d194948b26f46d1e2e35ab2c0
Nddy Andy <iam.ndie@gmail.com>
2017-11-08 03:38:22 +0100

    heroku deployment111

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() == 'production') {
-            URL::forceScheme('https');
-        }
+       
     }
-    
+    public function boot()
+    {
+        if($this->app->environment() === 'production'){ $this->app['request']->server->set('HTTPS', true); }
+    }
 }
