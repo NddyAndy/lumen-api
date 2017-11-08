09bbe5dedb8f426e16bf6695807ce00e45bc774f
Nddy Andy <iam.ndie@gmail.com>
2017-11-08 10:23:31 +0100

    deployment stuff two

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,5 +19,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if($this->app->environment() === 'production'){ $this->app['request']->server->set('HTTPS', true); }
+        Schema::defaultStringLength(191);
     }
 }
